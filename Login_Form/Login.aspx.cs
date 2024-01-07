8b4b299e992e542372763bba94fc1dc49bc03e61
livinjava <155757114+livinjava@users.noreply.github.com>
2024-01-07 09:55:47 +0530

    first

---
+++ Login_Form/Login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Login_Form
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
